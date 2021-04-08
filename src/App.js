2a1fea432a90694c63d75e62db65cea319891a34
nestar01 <toqo1912@gmailcom>
2021-04-09 00:05:36 +0900

    seconde commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import main_logo from './flower_main.png';
 import './App.css';
 
+
 function App() {
+
+  let posts = "ㅇㅇㅇ";
+  let date = "2021-04-07";
+
+  //state : 변수 대신 쓰는 데이터 저장공간, 변경시 새로고침 없이 html 재렌더링, webapp처럼 동작
+  let [글제목, 글제목변경] = useState(['폰트', '템플릿', '그림']);  
+   
+  function funct(){
+    return 200;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+       <div className="black-nav">
+         <div>개발 Blog</div>
+       </div>
+       <img className="main_logo" src={main_logo}></img>
+
+       <div className="list">
+          <h3>{글제목[0]}</h3>
+          <p>{date}</p>
+          <hr/>
+       </div>
     </div>
   );
 }
+
+
 
 export default App;
